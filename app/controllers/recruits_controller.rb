2aa3ea0d27a5bd9deeaa595f62528018b7931fcf
minamihiroto <kokowatamf@gmail.com>
2020-01-20 20:17:18 +0900

    ライブ募集の詳細

--- app/controllers/recruits_controller.rb
+++ app/controllers/recruits_controller.rb
@@ -4,6 +4,10 @@ class RecruitsController < ApplicationController
     @recruits = Recruit.all.order(created_at: :desc)
   end
   
+  def show
+    @recruit = Recruit.find_by(id: params[:id])
+  end
+
   def new      
     @recruit = Recruit.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   
 get "recruits/index"
 get "recruits/new"
+get "recruits/:id" => "recruits#show"
 end
